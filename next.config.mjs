331de94fe611c9a4aa6586e7dcfec84ed701f8ea
rishabh-star <risingmerta@gmail.com>
2024-08-11 06:26:50 -1200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['static-assets-44d.pages.dev', 'static-assets.freeanimehentai.net', 'hentai.tv'], // Add your domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true, // This will disable ESLint during the build process
+  },
 };
 
 export default nextConfig;
