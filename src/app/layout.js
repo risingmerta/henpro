bcc7f9cec9333e3f95bc598c135d0e5b5ac071c8
rishabh-star <risingmerta@gmail.com>
2024-08-12 00:38:11 -1200

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from "next/dynamic";
+import Script from "next/script";
+
 const DynamicNavic = dynamic(() => import("@/app/Nav/page"), {
   ssr: false,
 });
@@ -8,8 +10,7 @@ const DynamicNavic = dynamic(() => import("@/app/Nav/page"), {
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title:
-    "Watch Free Hentai Video Streams Online in 720p , 1080p HD - hanime.tv",
+  title: "Watch Free Hentai Video Streams Online in 720p , 1080p HD - hanime.tv",
   description: `Enjoy your unlimited hentai & anime
           collection. We are the definitive source for the best curated 720p /
           1080p HD hentai videos, viewable by mobile phone and tablet, for free.`,
@@ -18,12 +19,31 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-64QSGGL3N5"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-64QSGGL3N5');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
-        <DynamicNavic>{children}</DynamicNavic>
+        <DynamicNavic />
+        {children}
       </body>
     </html>
   );
