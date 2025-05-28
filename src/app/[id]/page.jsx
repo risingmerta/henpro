3b3777c48b3cef24088c072249269c8ad5279243
rishabh-star <risingmerta@gmail.com>
2025-05-28 16:17:16 +0530

    first commit

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,45 +1,110 @@
+import React from "react";
+import "./page.css";
+import Link from "next/link";
+import { connectDB } from "@/lib/mongoClient";
 import { MongoClient } from "mongodb";
-import { Suspense } from "react";
 import WatchPageClient from "@/components/WatchPageClient/WatchPageClient";
 
 export async function generateMetadata({ params }) {
-  const formattedTitle = params.id
-    .split("-")
-    .map((w) => w[0].toUpperCase() + w.slice(1))
-    .join(" ");
+  // let data = [];
+  // const ids = params.id;
+  // const id = "/" + ids + "/";
+  // const db = await connectDB();
+  // try {
+  //   const animeInfoCol = db.collection("hentai");
+  //   const doc = await animeInfoCol.findOne({
+  //     _id: "/fuuki-iin-to-fuuzoku-katsudou-episode-2/",
+  //   });
+  //   if (doc) {
+  //     data = doc;
+  //   } else {
+  //     data = "no didnt";
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching document:", error);
+  // }
+
+  // console.log("data", data);
+
+  function capitalizeWords(str) {
+    return str
+      .split("-")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+
+  const paramsId = params.id;
+  const formattedTitle = capitalizeWords(paramsId);
+
+  console.log(formattedTitle);
+  // Output: Fuuki Iin To Fuuzoku Katsudou Episode 2
 
   return {
     title: `Watch ${formattedTitle} Hentai Video Streams Online in 720p , 1080p HD - henpro`,
-    description: `Enjoy your unlimited hentai & anime collection in HD, mobile-optimized for free.`,
+    description: `Enjoy your unlimited hentai & anime
+          collection. We are the definitive source for the best curated 720p /
+          1080p HD hentai videos, viewable by mobile phone and tablet, for free.`,
   };
 }
 
-export default async function Page({ params }) {
-  const id = "/" + params.id + "/";
-
-  const uri = "mongodb://animoon:Imperial_merta2030@145.223.82.113:27017/?authSource=admin";
-  const client = new MongoClient(uri);
-  let data = null;
+export default async function page({ params }) {
+  let imageURL = "";
+  let data = [];
   let datal = [];
+  const ids = params.id;
+  const id = "/" + ids + "/";
+  // const db = await connectDB();
+  console.log("params.id:", params.id);
+  console.log("Querying _id:", `/${params.id}/`);
+
+  const uri =
+    "mongodb://animoon:Imperial_merta2030@145.223.82.113:27017/?authSource=admin";
+  const client = new MongoClient(uri);
 
   try {
     await client.connect();
     const db = client.db("mydatabase");
-    const doc = await db.collection("hentai").findOne({ _id: id });
+    const collection = db.collection("hentai");
+
+    // const id = "/fuuki-iin-to-fuuzoku-katsudou-episode-2/";
+
+    const doc = await collection.findOne({ _id: id });
     data = doc;
+    imageURL = doc.poster;
+
+    if (doc) {
+      console.log("Document found:", doc.title);
+    } else {
+      console.log("Document not found.");
+    }
+  } catch (e) {
+    console.error(e);
   } finally {
     await client.close();
   }
 
-  const apiRes = await fetch("https://hent.shoko.fun/api/hen-random", {
+  // try {
+  //   const animeInfoCol = db.collection("hentai");
+  //   const doc = await animeInfoCol.findOne({
+  //     _id: "/fuuki-iin-to-fuuzoku-katsudou-episode-2/",
+  //   });
+  //   data = doc;
+  //   if (doc.poster) {
+  //     imageURL = doc.poster;
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching document:", error);
+  // }
+  // Fetch data from API
+  const apiResponse = await fetch(`https://hent.shoko.fun/api/hen-random`, {
     cache: "no-store",
   });
-  const apiJson = await apiRes.json();
-  datal = apiJson.results.data;
-
+  const newResponse = await apiResponse.json();
+  const apiData = newResponse.results.data;
+  datal = apiData;
   return (
-    <Suspense>
+    <>
       <WatchPageClient data={data} datal={datal} />
-    </Suspense>
+    </>
   );
 }
--- src/components/WatchPageClient/WatchPageClient.jsx
+++ src/components/WatchPageClient/WatchPageClient.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import NavSidebar from "@/components/NavSideBar/NavSideBar";
 import Footer from "@/components/footer/Footer";
 import Link from "next/link";
-import './watch.css'
+import "./watch.css";
 
 export default function WatchPageClient({ data, datal }) {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -21,88 +21,125 @@ export default function WatchPageClient({ data, datal }) {
 
   return (
     <>
-      <Navbar isScrolled={isScrolled} sidebarIsOpen={sidebarIsOpen} setSidebarIsOpen={setSidebarIsOpen} />
-      <NavSidebar sidebarIsOpen={sidebarIsOpen} setSidebarIsOpen={setSidebarIsOpen} />
+      <Navbar
+        isScrolled={isScrolled}
+        sidebarIsOpen={sidebarIsOpen}
+        setSidebarIsOpen={setSidebarIsOpen}
+      />
+      <NavSidebar
+        sidebarIsOpen={sidebarIsOpen}
+        setSidebarIsOpen={setSidebarIsOpen}
+      />
       <div className="compli">
         <div className="watc">
-          <iframe
-            src={data.url}
-            frameBorder="0"
-            className="ifro"
-            allowFullScreen
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
-          <div className="slab1">
-            <div className="titleD">{data.title}</div>
-            <div className="viewD">{data.views}</div>
-          </div>
-          <div className="slab2">
-            <div className="fourCon">
-              <div className="postCon">
-                <img src={data.poster} alt="" className="posterD" />
-              </div>
-              <div className="fourA">
-                {/* Brand and Censor Info */}
-                <div className="flex four1">
-                  <div className="four11">
-                    <div className="teamA">Brand</div>
-                    <div className="brando">{data.info?.brand}</div>
-                  </div>
-                  <div className="four12">
-                    {data.cencored && <div className="cencored">{data.cencored}</div>}
-                  </div>
-                </div>
-                {/* Other Info */}
-                <div className="four2">
-                  <div className="teamA">Brand Uploads</div>
-                  <div className="restInfo">{data.info?.brandUploads}</div>
-                </div>
-                <div className="flex four3">
-                  <div className="four31">
-                    <div className="teamA">Release Date</div>
-                    <div className="restInfo">{data.info?.releasedDate}</div>
-                  </div>
-                  <div className="four32">
-                    <div className="teamA">Upload Date</div>
-                    <div className="restInfo">{data.info?.uploadDate}</div>
-                  </div>
-                </div>
-                <div className="four4">
-                  <div className="teamA">Alternate Title</div>
-                  <div className="restInfo">{data.info?.alternateTitle}</div>
-                </div>
-              </div>
+          <div>
+            <div>
+              <iframe
+                src={data.url}
+                frameBorder="0"
+                className="ifro"
+                allowFullScreen
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
             </div>
-            <div className="descSlab">
-              <div className="tagG">
-                {data.moreInfo.tags.map((tag, idx) => (
-                  <Link
-                    key={idx}
-                    className="tags"
-                    href={`/tags/genre?item=${tag.replace(/\s+/g, "-").toLowerCase()}`}
-                  >
-                    {tag}
-                  </Link>
-                ))}
+            <div>
+              <div className="slab1">
+                <div className="titleD">{data.title}</div>
+                <div className="viewD">{data.views}</div>
               </div>
-              <div className="desc1">{data.moreInfo.descripOne}</div>
-              {data.moreInfo.descripOne !== data.moreInfo.descripTwo && (
-                <div className="desc2">{data.moreInfo.descripTwo}</div>
-              )}
+              <div className="slab2">
+                <div className="fourCon">
+                  <div className="postCon">
+                    <img src={data.poster} alt="" className="posterD" />
+                  </div>
+                  <div className="fourA">
+                    <div className="flex four1">
+                      <div className="four11">
+                        <div>
+                          <div className="teamA">Brand</div>
+                          <div className="brando">{data.info?.brand}</div>
+                        </div>
+                      </div>
+                      <div className="four12">
+                        {data.cencored ? (
+                          <div className="cencored">{data.cencored}</div>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+                    <div className="four2">
+                      <div className="teamA">Brand Uploads</div>
+                      <div className="restInfo">{data.info?.brandUploads}</div>
+                    </div>
+                    <div className="flex four3">
+                      <div className="four31">
+                        <div>
+                          <div className="teamA">Release Date</div>
+                          <div className="restInfo">
+                            {data.info?.releasedDate}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="four32">
+                        <div>
+                          <div className="teamA">Upload Date</div>
+                          <div className="restInfo">
+                            {data.info?.uploadDate}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="four4">
+                      <div className="teamA">Alternate Title</div>
+                      <div className="restInfo">
+                        {data.info?.alternateTitle}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="descSlab">
+                  <div className="tagG">
+                    {data.moreInfo.tags.map((i) => (
+                      <Link
+                        className="tags"
+                        href={`/tags/genre?item=${i
+                          .replace(" ", "-")
+                          .toLowerCase()}`}
+                      >
+                        {i}
+                      </Link>
+                    ))}
+                  </div>
+                  <div className="desc1">{data.moreInfo.descripOne}</div>
+                  <div className="desc2">
+                    {data.moreInfo.descripOne !== data.moreInfo.descripTwo
+                      ? data.moreInfo.descripTwo
+                      : ""}
+                  </div>
+                </div>
+              </div>
+              <div></div>
             </div>
           </div>
-          <div className="kalu">
-            {datal.map((item) => (
-              <Link key={item.id} className="alliu" href={item.id}>
-                <div className="fixed-size-container">
-                  <img src={item.banner} alt={item.title} className="fixed-size-img" />
-                </div>
-                <div className="iopu">
-                  <div className="titleo">{item.title}</div>
-                  <div className="lopi">{item.views}</div>
-                </div>
-              </Link>
-            ))}
+          <div>
+            <div className="kalu">
+              {datal.map((i) => (
+                <Link className="alliu" href={i.id}>
+                  <div className="fixed-size-container">
+                    <img
+                      src={i.banner}
+                      alt={i.title}
+                      className="fixed-size-img"
+                    />
+                  </div>
+                  <div className="iopu">
+                    <div className="titleo">{i.title}</div>
+                    <div className="lopi">{i.views}</div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
